2bf5674f780874a34437674232603755f3dd146a
Goran Silic <goransilic11@gmail.com>
2018-05-30 23:34:34 +0200

    added some components

---
+++ src/app/pages/step5/step5.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step5',
+  templateUrl: './step5.component.html',
+  styleUrls: ['./step5.component.less']
+})
+export class Step5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
